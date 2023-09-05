9b3dc9bfc1278326d1c90431e5a82e4c9bf765c4
shima <oty998@gmail.com>
2023-09-05 18:34:20 +0300

    test

--- src/core/prodaction.js
+++ src/core/prodaction.js
@@ -1,23 +1,8 @@
 const VERCEL_URL = `${process.env.VERCEL_URL}`;
 
 const production = async (
-  req,
-  res,
   bot
 ) => {
-
-  if (!VERCEL_URL) {
-    throw new Error('VERCEL_URL is not set.');
-  }
-
-  const getWebhookInfo = await bot.telegram.getWebhookInfo();
-  if (getWebhookInfo.url !== VERCEL_URL + '/api') {
-    await bot.telegram.deleteWebhook();
-    await bot.telegram.setWebhook(`${VERCEL_URL}/api`);
-  }
-
-  // Start listening for incoming updates
-  await bot.startWebhook('/api', null, 3000); // Replace with the desired port
   // try {
   //   if (!(req.method === 'POST')) {
   //     res.status(200).json('Listening to bot events...');
