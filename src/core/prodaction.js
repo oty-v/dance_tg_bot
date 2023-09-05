c661e98fce8606f842262b461633f1c4a5325e5d
shima <oty998@gmail.com>
2023-09-05 18:35:01 +0300

    test

--- src/core/prodaction.js
+++ src/core/prodaction.js
@@ -3,14 +3,25 @@ const VERCEL_URL = `${process.env.VERCEL_URL}`;
 const production = async (
   bot
 ) => {
-  // try {
-  //   if (!(req.method === 'POST')) {
-  //     res.status(200).json('Listening to bot events...');
-  //   }
-  // } catch (error) {
-  //   console.log('Error:', error);
-  //   res.status(500).end('Internal Server Error');
-  // }
+
+  if (!VERCEL_URL) {
+    throw new Error('VERCEL_URL is not set.');
+  }
+
+  const getWebhookInfo = await bot.telegram.getWebhookInfo();
+  if (getWebhookInfo.url !== VERCEL_URL + '/api') {
+    await bot.telegram.deleteWebhook();
+    await bot.telegram.setWebhook(`${VERCEL_URL}/api`);
+  }
+
+  try {
+    if (!(req.method === 'POST')) {
+      res.status(200).json('Listening to bot events...');
+    }
+  } catch (error) {
+    console.log('Error:', error);
+    res.status(500).end('Internal Server Error');
+  }
 };
 
 module.exports = production;
